27bb3a2e8b0e7af3f74b63836aec6dfe4649d96f
Mariano Benitez <mariano@benitez.nu>
2014-09-03 21:19:30 -0300

    inicial

---
+++ tweet-wars-server/src/main/java/ar/edu/itba/pod/mmxivii/App.java
@@ -0,0 +1,13 @@
+package ar.edu.itba.pod.mmxivii;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
