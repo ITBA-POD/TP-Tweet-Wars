2f0abc2773f05f0b22802d134116cc88fca615a7
Mariano Benitez <mariano@tekgenesis.com>
2014-09-04 09:27:50 -0300

    game master registra tweets

--- tweet-wars-server/src/main/java/ar/edu/itba/pod/mmxivii/tweetwars/GameMaster.java
+++ tweet-wars-server/src/main/java/ar/edu/itba/pod/mmxivii/tweetwars/GameMaster.java
@@ -3,13 +3,12 @@ package ar.edu.itba.pod.mmxivii.tweetwars;
 import javax.annotation.Nonnull;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.Collection;
 
 public interface GameMaster extends Remote
 {
-	void newPlayer(@Nonnull GamePlayer player);
+	void newPlayer(@Nonnull GamePlayer player, @Nonnull String hash);
 
 	void tweetReceived(@Nonnull GamePlayer player, @Nonnull Status tweet) throws RemoteException;
 
-	void tweetsReceived(@Nonnull GamePlayer player, @Nonnull Collection<Status> tweets) throws RemoteException;
+	void tweetsReceived(@Nonnull GamePlayer player, @Nonnull Status[] tweets) throws RemoteException;
 }
