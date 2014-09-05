2f12bb2734388c7231b5d141d2ad91a888eff168
Mariano Benitez <mariano@tekgenesis.com>
2014-09-05 08:17:34 -0300

    cliente ejemplo

---
+++ tweet-wars-sample-player/src/main/java/ar/edu/itba/pod/mmxivii/alumno/App.java
@@ -0,0 +1,13 @@
+package ar.edu.itba.pod.mmxivii.alumno;
+
+public class App
+{
+	private App()
+	{
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hola alumno!");
+	}
+}
---
+++ tweet-wars-sample-player/src/test/java/ar/edu/itba/pod/mmxivii/alumno/TestApp.java
@@ -0,0 +1,13 @@
+package ar.edu.itba.pod.mmxivii.alumno;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestApp
+{
+	@Test public void test()
+	{
+		assertThat(true).isTrue();
+	}
+}
